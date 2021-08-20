572b75e3f4ac3603a7bf238fcfcd590dd84756d7
miya-start <0000000to1@gmail.com>
2021-08-20 20:42:15 +0900

    Add secure response headers
    https://youtu.be/wOaa3gxRifo
    https://github.com/jagaapple/next-secure-headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+        ],
+      },
+    ]
+  },
   i18n: {
     locales: ['ja'],
     defaultLocale: 'ja',
