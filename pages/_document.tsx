4c17c973ba3d153be040347ac93efe986b197466
miya-start <0000000to1@gmail.com>
2021-08-29 08:04:19 +0900

    Remove nonce for CSP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,15 @@
-import crypto from 'crypto'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const cspHashOf = (text: string) => {
-  const hash = crypto.createHash('sha256')
-  hash.update(text)
-  return `'sha256-${hash.digest('base64')}'`
-}
 export default class MyDocument extends Document {
   render() {
-    const nonce = cspHashOf(NextScript.getInlineScriptSource(this.props))
     let csp = `default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:`
     if (process.env.NODE_ENV !== 'production') {
-      csp = `default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; script-src 'self' 'unsafe-eval'`
+      csp += `default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; script-src 'self' 'unsafe-eval'`
     }
 
     return (
       <Html>
-        <Head nonce={nonce}>
+        <Head>
           <meta httpEquiv="Content-Security-Policy" content={csp} />
         </Head>
         <body>
